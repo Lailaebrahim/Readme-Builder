39dee6256e85820c4a6025aa1065923743877b79
lailaebrahim <ebrahimlaila815@gmail.com>
2024-07-12 04:34:52 +0300

    Edit models to allow user have an image with a default and add media urls in app urls

--- ReadmeBuilderWebProject/ReadmeBuilderWebApp0/models.py
+++ ReadmeBuilderWebProject/ReadmeBuilderWebApp0/models.py
@@ -4,10 +4,14 @@ from django.db import models
 class User(models.Model):
     id = models.IntegerField(primary_key=True)
     username = models.CharField(max_length=100, unique=True, null=False)
+    image = models.ImageField(upload_to='images/', default='media/default.jpg')
     email = models.CharField(max_length=256, unique=True, null=False)
     password = models.CharField(max_length=128, null=False)
     github_username = models.CharField(max_length=256, unique=True, null=False)
     github_token = models.CharField(max_length=256)
+    
+    def __str__(self) -> str:
+        return self.username
 
 
 class ReadmeFile(models.Model):
