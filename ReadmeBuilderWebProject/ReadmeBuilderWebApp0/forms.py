7711b5785d1a1492d5221e590afe5c97ca85db69
lailaebrahim <ebrahimlaila815@gmail.com>
2024-07-19 23:55:28 +0300

    Create forms file and User sign in form

--- ReadmeBuilderWebProject/ReadmeBuilderWebApp0/forms.py
+++ ReadmeBuilderWebProject/ReadmeBuilderWebApp0/forms.py
@@ -1,23 +1,49 @@
-from django.contrib.auth.forms import UserCreationForm
+# Import necessary modules
 from django import forms
-from .models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .utils import is_valid_github_username
 
-
+# Define the SignUpForm class
 class SignUpForm(UserCreationForm):
+    """
+    A form for user sign up with additional fields for email and GitHub username.
+    """
+
+    # Define form fields
     email = forms.EmailField(label='Email', max_length=256)
     github_username = forms.CharField(label='GitHub Username', max_length=256)
 
     class Meta(UserCreationForm.Meta):
+        # Include additional fields in the form
         fields = UserCreationForm.Meta.fields + ('email', 'github_username',)
 
     def clean_email(self):
+        """
+        Clean and validate the email field.
+
+        Raises:
+            forms.ValidationError: If the email is already taken.
+
+        Returns:
+            str: The cleaned email.
+        """
         email = self.cleaned_data.get('email')
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError('Email is already taken.')
         return email
 
     def clean_github_username(self):
+        """
+        Clean and validate the GitHub username field.
+
+        Raises:
+            forms.ValidationError: If the GitHub username is associated with another account
+                                   or if it is invalid.
+        
+        Returns:
+            str: The cleaned GitHub username.
+        """
         github_username = self.cleaned_data.get('github_username')
         if User.objects.filter(github_username=github_username).exists():
             raise forms.ValidationError(
