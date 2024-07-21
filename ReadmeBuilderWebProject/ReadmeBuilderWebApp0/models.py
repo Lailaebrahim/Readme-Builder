f91e69b250890512c3c2143e45cf260a187e5182
lailaebrahim <ebrahimlaila815@gmail.com>
2024-07-21 19:19:32 +0300

    Update db models to use built-in User model and extend it by one to one relationship

--- ReadmeBuilderWebProject/ReadmeBuilderWebApp0/models.py
+++ ReadmeBuilderWebProject/ReadmeBuilderWebApp0/models.py
@@ -1,20 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
-class User(models.Model):
-    id = models.IntegerField(primary_key=True)
-    username = models.CharField(max_length=100, unique=True, null=False)
-    image = models.ImageField(upload_to='images/', default='media/default.jpg')
-    email = models.CharField(max_length=256, unique=True, null=False)
-    password = models.CharField(max_length=128, null=False)
-    github_username = models.CharField(max_length=256, unique=True, null=False)
-    github_token = models.CharField(max_length=256)
-    
+class ReadmeWritterUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(
+        upload_to='images/', default='images/default.jpg')
+    github_username = models.CharField(max_length=256, null=False)
+    github_token = models.CharField(max_length=256, null=True)
+
     def __str__(self) -> str:
-        return self.username
+        return self.github_username
 
 
-class ReadmeFile(models.Model):
-    id = models.IntegerField(primary_key=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField(null=False)
+@receiver(post_save, sender=User)
+def create_user_ReadmeWritterUser(sender, instance, created, **kwargs):
+    if created:
+        ReadmeWritterUser.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_ReadmeWritterUser(sender, instance, **kwargs):
+    instance.readmewritteruser.save()
+
+
+class ReadmeFiles(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='ReadmeFiles/', null=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"ReadmeFile {self.id} of User {self.user.username}"
--- ReadmeBuilderWebProject/ReadmeBuilderWebApp0/views.py
+++ ReadmeBuilderWebProject/ReadmeBuilderWebApp0/views.py
@@ -1,11 +1,39 @@
 from django.template import loader
 from django.http import HttpResponse
+from .models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import make_password
+from django.contrib import messages
+from .forms import SignUpForm
 
 
 def home(request):
-    template = loader.get_template('ReadmeBuilderWebApp0/base.html')
+    template = loader.get_template('ReadmeBuilderWebApp0/home.html')
     return HttpResponse(template.render(context={}, request=request))
 
 
-def about(request):
-    return HttpResponse("Hello, world From Readme Writter about!")
+def account(request):
+    template = loader.get_template('ReadmeBuilderWebApp0/user_account.html')
+    return HttpResponse(template.render(context={}, request=request))
+
+
+def signin(request):
+    template = loader.get_template('ReadmeBuilderWebApp0/signin.html')
+    return HttpResponse(template.render(context={}, request=request))
+
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = User(username=form.cleaned_data.get('username'),
+                        email=form.cleaned_data.get('email'),
+                        github_username=form.cleaned_data.get(
+                            'github_username'),
+                        password=make_password(form.cleaned_data.get('password1')))
+            user.save()
+            messages.success(request, 'Your account has been created successfully!')
+            return redirect('signin')
+    else:
+        form = SignUpForm()
+    return render(request, "ReadmeBuilderWebApp0/signup.html", {"form": form})
